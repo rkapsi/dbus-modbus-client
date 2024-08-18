04682e897f4d30b90a5ef477298cc685787cb5ce
Roger Kapsi <rkapsi@gmail.com>
2024-08-18 07:23:45 +0300

    This may work

--- shelly.py
+++ shelly.py
@@ -4,22 +4,14 @@ import device
 import probe
 from register import *
 
-log = logging.getLogger(__name__)
-
-
-class ShellyEnergyMeter(device.CustomName, device.EnergyMeter):
-    # Define mandatory properties so the device gets saved
-    productid = 0xFFFF
-    productname = 'Shelly Energy Meter'
+class Shelly_Meter(device.CustomName, device.EnergyMeter):
+    vendor_id = 'shelly'
+    vendor_name = 'Shelly'
 
     min_timeout = 0.5
-    # Shelly Modbus devices oddly use input registers for everything
     default_access = 'input'
-    nr_phases = 3
 
     def device_init(self):
-        log.info('Initializing Shelly energy meter using connection "%s"', self.connection())
-
         self.info_regs = [
             Reg_text(0, 6, '/Serial', little=True),
             Reg_text(6, 10, '/ProductName', little=True),
@@ -29,33 +21,49 @@ class ShellyEnergyMeter(device.CustomName, device.EnergyMeter):
             Reg_f32l(1162, '/Ac/Energy/Forward', 1000, '%.1f kWh'),
             Reg_f32l(1164, '/Ac/Energy/Reverse', 1000, '%.1f kWh'),
             Reg_f32l(1013, '/Ac/Power', 1, '%.1f W'),
-            Reg_f32l(1182, '/Ac/L1/Energy/Forward', 1000, '%.1f kWh'),
-            Reg_f32l(1184, '/Ac/L1/Energy/Reverse', 1000, '%.1f kWh'),
+
             Reg_f32l(1020, '/Ac/L1/Voltage', 1, '%.1f V'),
             Reg_f32l(1022, '/Ac/L1/Current', 1, '%.1f A'),
-            Reg_f32l(1024, '/Ac/L1/Power', 1, '%.1f W'),
-            Reg_f32l(1202, '/Ac/L2/Energy/Forward', 1000, '%.1f kWh'),
-            Reg_f32l(1204, '/Ac/L2/Energy/Reverse', 1000, '%.1f kWh'),
-            Reg_f32l(1040, '/Ac/L2/Voltage', 1, '%.1f V'),
-            Reg_f32l(1042, '/Ac/L2/Current', 1, '%.1f A'),
-            Reg_f32l(1044, '/Ac/L2/Power', 1, '%.1f W'),
-            Reg_f32l(1222, '/Ac/L3/Energy/Forward', 1000, '%.1f kWh'),
-            Reg_f32l(1224, '/Ac/L3/Energy/Reverse', 1000, '%.1f kWh'),
-            Reg_f32l(1060, '/Ac/L3/Voltage', 1, '%.1f V'),
-            Reg_f32l(1062, '/Ac/L3/Current', 1, '%.1f A'),
-            Reg_f32l(1064, '/Ac/L3/Power', 1, '%.1f W'),
+
         ]
         
     def get_ident(self):
         return 'shelly_{}'.format(self.info['/Serial'])
 
 
-class ShellyModelRegister(probe.ModelRegister):
-    def __init__(self, **args):
-        super().__init__(None, [], **args)
+class Shelly_Pro_3EM(Shelly_Meter):
+    productname = 'Shelly Pro 3EM'
+    productid = 0xFFFF
+    nr_phases = 3
 
-    def probe(self, spec, modbus, timeout=None):
-        return ShellyEnergyMeter(spec, modbus, 'SPEM-003CE')
+    def device_init(self):
+        super().device_init()
 
+        self.data_regs += [
+            Reg_f32l(1182, '/Ac/L1/Energy/Forward', 1000, '%.1f kWh'),
+            Reg_f32l(1184, '/Ac/L1/Energy/Reverse', 1000, '%.1f kWh'),
+            Reg_f32l(1024, '/Ac/L1/Power', 1, '%.1f W'),
 
-probe.add_handler(ShellyModelRegister(methods=['tcp'], units=[1]))
+            Reg_f32l(1040, '/Ac/L2/Voltage', 1, '%.1f V'),
+            Reg_f32l(1042, '/Ac/L2/Current', 1, '%.1f A'),
+            Reg_f32l(1202, '/Ac/L2/Energy/Forward', 1000, '%.1f kWh'),
+            Reg_f32l(1204, '/Ac/L2/Energy/Reverse', 1000, '%.1f kWh'),
+            Reg_f32l(1044, '/Ac/L2/Power', 1, '%.1f W'),
+
+            Reg_f32l(1060, '/Ac/L3/Voltage', 1, '%.1f V'),
+            Reg_f32l(1062, '/Ac/L3/Current', 1, '%.1f A'),
+            Reg_f32l(1222, '/Ac/L3/Energy/Forward', 1000, '%.1f kWh'),
+            Reg_f32l(1224, '/Ac/L3/Energy/Reverse', 1000, '%.1f kWh'),
+            Reg_f32l(1064, '/Ac/L3/Power', 1, '%.1f W'),
+        ]
+
+models = {
+    Shelly_Pro_3EM.productid: {
+        'model':    'SPEM-003CE',
+        'handler':  Shelly_Pro_3EM,
+    },
+}
+
+probe.add_handler(probe.ModelRegister(Reg_u16(0x2000), models,
+                                      methods=['tcp'],
+                                      units=[1]))
